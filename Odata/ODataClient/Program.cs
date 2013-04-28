f9a99eaadc3ecf6e035429fea3af545812ede414
William <willjlee@fake.email>
2013-04-27 20:13:55 -0400

    First commit. Service and client.

---
+++ Odata/ODataClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODataClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new ODataService.Container(
+            new Uri("http://localhost:55176/odata/"));
+
+            container.SendingRequest2 += (s, e) =>
+            {
+                Console.WriteLine("{0} {1}",
+                    e.RequestMessage.Method, e.RequestMessage.Url);
+            };
+
+            // Get all products
+            foreach (var product in container.Products)
+            {
+                Console.WriteLine("{0} {1} {2}", product.Name, product.Price,
+                    product.Category);
+            }
+
+            // Select products
+            var products = from p in container.Products where p.ID == 1 select p;
+            var product1 = products.FirstOrDefault();
+            if (product1 != null)
+            {
+                Console.WriteLine("{0} {1} {2}", product1.Name, product1.Price,
+                    product1.Category);
+            }
+
+            Console.ReadLine(); //hold console open
+        }
+    }
+}
---
+++ Odata/Odata/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Odata.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.OData;
+
+namespace Odata.Controllers
+{
+    public class ProductsController : EntitySetController<Product, int>
+    {
+        static List<Product> products = new List<Product>() //normally this would be a database
+        {
+            new Product() { ID = 1, Name = "Hat", Price = 15, Category = "Apparel" },
+            new Product() { ID = 2, Name = "Socks", Price = 5, Category = "Apparel" },
+            new Product() { ID = 3, Name = "Scarf", Price = 12, Category = "Apparel" },
+            new Product() { ID = 4, Name = "Yo-yo", Price = 4.95M, Category = "Toys" },
+            new Product() { ID = 5, Name = "Puzzle", Price = 8, Category = "Toys" },
+        };
+
+        public override IQueryable<Product> Get()
+        {
+            return products.AsQueryable();
+        }
+
+        protected override Product GetEntityByKey(int key)
+        {
+            return products.FirstOrDefault(p => p.ID == key);
+        }
+    }
+}
